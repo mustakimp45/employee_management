29e4904b532cc87f09461a57baa7402a0b38e394
MPendari <mustakim.pendari@estuate.com>
2022-11-03 16:00:36 +0530

    First Commit

---
+++ src/main/java/com/employeedatabase/EmployeeDatabaseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.employeedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDatabaseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDatabaseManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeedatabase/EmployeeDatabaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDatabaseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
