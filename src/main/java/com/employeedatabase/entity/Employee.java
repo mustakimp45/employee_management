83b25b3643941b0d69714525e46e56f62157f38e
Ajay Kumar D <115684961+AjayGowdaaaa@users.noreply.github.com>
2022-11-03 16:41:34 +0530

    Ajay Committed

--- src/main/java/com/employeedatabase/entity/Employee.java
+++ src/main/java/com/employeedatabase/entity/Employee.java
@@ -2,25 +2,20 @@ package com.employeedatabase.entity;
 
 import java.util.Date;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Employee {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long empId;
-
     private String firstName;
-
-
     private String lastName;
-
-
     private Date dateOfBirth;
-
-
     private String email;
-
-
     private Long phoneNumber;
-
-
     private String photoPath;
 
 
